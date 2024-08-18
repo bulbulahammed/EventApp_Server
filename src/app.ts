c8ab60b8b2b9110e3fec2fe603a223f7a40eb2a9
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2024-08-18 23:22:20 +0600

    Setup Husky

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
+import express, { Application, Request } from 'express'
 const app: Application = express()
 
 // Cors
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello, Event App Server')
 })
 
