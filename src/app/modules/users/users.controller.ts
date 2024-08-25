dd3bef2b050fcf94946c44afe2a8279f0b6b9235
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2024-08-25 23:13:30 +0600

    Create user

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Request, RequestHandler, Response } from 'express'
 import httpStatus from 'http-status'
